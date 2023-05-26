05fbe714bb5c7916a49d185948952fdd5b717acb
Summer Watts <summerw1008@icloud.com>
2023-05-25 18:13:29 -0700

    message

---
+++ src/Mine/Sum.java
@@ -0,0 +1,13 @@
+package Mine;
+
+
+
+public class Sum {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub		
+		
+		String originalString = "  This is an example string   ";
+		String strippedString = originalString.strip(); 
+	}
+	}
